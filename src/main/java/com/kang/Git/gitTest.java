0dd18dfe02747d5e113a192b030a9f153517eb34
kxwup <102808452+kxwup@users.noreply.github.com>
2022-04-02 19:15:08 +0800

    pull commit

--- src/main/java/com/kang/Git/gitTest.java
+++ src/main/java/com/kang/Git/gitTest.java
@@ -8,6 +8,8 @@ public class gitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
-
+        System.out.println("pull test");
+        
+        
     }
 }
