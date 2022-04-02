547ebd4004ec632d5bbdf85817e2eeee2d0fe268
kang <kxw5080@163.com>
2022-04-02 19:07:37 +0800

    push commit

--- src/main/java/com/kang/Git/gitTest.java
+++ src/main/java/com/kang/Git/gitTest.java
@@ -7,6 +7,7 @@ public class gitTest {
         System.out.println("hello33333,git3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
 
     }
 }
